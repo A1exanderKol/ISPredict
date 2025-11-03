73b55501ad82934e331a871e75230bb232602364
A1exanderKol <mr.kolesov.kol@gmail.com>
2025-11-03 22:52:57 +0700

    add facts.py

---
+++ facts.py
@@ -0,0 +1,17 @@
+from experta import *
+
+class ProjectFact(Fact):
+    """Факт о проекте"""
+    pass
+
+class LibraryFact(Fact):
+    """Факт о библиотеке"""
+    pass
+
+class RecommendationFact(Fact):
+    """Факт рекомендации"""
+    pass
+
+class WarningFact(Fact):
+    """Факт предупреждения"""
+    pass
